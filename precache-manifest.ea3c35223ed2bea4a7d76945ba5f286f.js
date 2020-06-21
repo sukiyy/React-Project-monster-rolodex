ab110203a71a13cec83daec5bf3a5c4baf56ecb3
sukiyy <sookie19931009@gmail.com>
2020-06-20 23:22:38 -0400

    Updates

---
+++ precache-manifest.ea3c35223ed2bea4a7d76945ba5f286f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cc00ac2f13804db93e8d42993f25ac1",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "54b97b565386cc5e406b",
+    "url": "/monster-rolodex/static/css/main.9e4a37f7.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54b97b565386cc5e406b",
+    "url": "/monster-rolodex/static/js/main.7c75013b.chunk.js"
+  },
+  {
+    "revision": "90506cc279e62bb14608",
+    "url": "/monster-rolodex/static/js/runtime-main.59621b72.js"
+  }
+]);
